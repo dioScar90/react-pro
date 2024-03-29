18e9d670b16c0339cbf3bef3b339c91200fc0a7c
Diogo L Scarmagnani <diogols@live.com>
2024-03-29 20:11:35 -0300

    persisting pagination

--- src/hooks/useFetchProducts/index.js
+++ src/hooks/useFetchProducts/index.js
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react"
 import { ProductsService } from "@/services/products"
 import { useQuery } from 'react-query'
+import { useNavigate, useSearchParams, useLocation } from 'react-router-dom'
 
 export const useFetchProducts = ({ perPage = 30 }) => {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const [searchParams] = useSearchParams()
   const [page, setPage] = useState(1)
   const [totalPages, setTotalPages] = useState(1)
   const { data, isLoading } = useQuery(['products', page], () => ProductsService.findAll(page, perPage))
@@ -13,12 +17,22 @@ export const useFetchProducts = ({ perPage = 30 }) => {
     }
 
     setPage(pageNumber)
+
+    const pathname = location.pathname
+    navigate(`${pathname}/?page=${pageNumber}`)
   }
   
   useEffect(() => {
     const value = data?.data.numberOfPages ?? 1
     setTotalPages(value)
   }, [data])
+
+  useEffect(() => {
+    const pageFromParams = +searchParams.get('page')
+    if (pageFromParams > 0) {
+      setPage(pageFromParams)
+    }
+  }, [searchParams])
 
   return { products: data?.data.products ?? [], isLoading, page, totalPages, changePage }
 }
